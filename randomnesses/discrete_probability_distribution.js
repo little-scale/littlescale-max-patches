d2580f33e16b1f04c8c7cdd3ed320987b177235c
little-scale <seb.tomczak@gmail.com>
2025-05-28 06:45:13 +0930

    Update discrete_probability_distribution.js

--- randomnesses/discrete_probability_distribution.js
+++ randomnesses/discrete_probability_distribution.js
@@ -1,1 +1,33 @@
+autowatch = 1;
 
+let lambda = 4.0; // average rate of events per interval
+
+function bang() {
+    const L = Math.exp(-lambda);
+    let p = 1.0;
+    let k = 0;
+
+    while (p > L) {
+        k++;
+        p *= Math.random();
+    }
+
+    outlet(0, k - 1);
+}
+
+function set(val) {
+    lambda = Math.max(0.0001, parseFloat(val)); // avoid zero
+}
+
+function msg_float(f) {
+    set(f);
+}
+
+function anything() {
+    if (messagename === "lambda") {
+        set(arguments[0]);
+    }
+}
+
+inlets = 1;
+outlets = 1;
